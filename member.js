5d8c42ae66ef60658428aee22d4b339695902cca
Nick <84195136+nickcampos06@users.noreply.github.com>
2023-10-15 04:09:01 +0000

    Second Copilot assist

---
+++ member.js
@@ -0,0 +1,9 @@
+function skilsMember() {
+  var skills = ['JavaScript', 'HTML', 'CSS', 'Python', 'C', 'C++', 'Java'];
+  var member = {
+    name: 'John Doe',
+    age: 34,
+    skills: skills
+  };
+  return member.skills;
+} 
